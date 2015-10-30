f1b854569a6675946a34c5554bee27921af106c6
Lei Xu <lxu@pivotal.io>
2015-10-31 02:29:41 +0900

    shin okubo house desc modified

--- dummy_data/10000001/translation/10000001_directions_shinokubo_home.js
+++ dummy_data/10000001/translation/10000001_directions_shinokubo_home.js
@@ -4,22 +4,22 @@ module.exports =
     
     "MENU": "Menu",
     "DIRECTIONS": "Directions",
-    "PAGETITLE": "Walking from the JR Shin-Okubo Station to the house",
+    "PAGETITLE": "Walking from the JR Okubo Station to the house",
     "PANELTITLE": "Walking Guide",
-    "GUIDE": "The apparent sign is the mini store. Walking distance is only 3-4 minutes."
+    "GUIDE": "Get out of the only exit of Shin Okubo Station, then make a right. After walking 200 meters you will see a convenience store called mini store. Get into the small lane for 70 meters you will find the house."
   },
   "zh-CN": {
     "MENU": "菜单",
     "DIRECTIONS": "导航",
-    "PAGETITLE": "从JR山手线新大久保车站步行到家",
+    "PAGETITLE": "从JR中央线大久保车站步行到家",
     "PANELTITLE": "步行导向",
-    "GUIDE": "mini store是关键路标，大约总共走几分钟。"
+    "GUIDE": "新大久保站只有一个出口。出口之外就是大路‘大久保通’，顺着大路往右走200米左右，可以在一个小路路口看到便利店mini store，然后顺着小路往里走几十米就可以到家，大约总共步行3至4分钟。地址是新宿区大久保2-32-9。"
   },
   "zh-HK": {
     "MENU": "菜單",
     "DIRECTIONS": "導航",
-    "PAGETITLE": "從JR山手線新大久保車站步行到家",
+    "PAGETITLE": "從JR中央線新大久保車站步行到家",
     "PANELTITLE": "步行導向",
-    "GUIDE": "mini store是关键路标，大約總共走幾分鐘。"
+    "GUIDE": "新大久保車站只有一個出口。走出出口，順著大路‘大久保通’往右步行200米左右，可以在一条小路路口看见便利店mini store，顺着小路往里步行几十米就可到达，大約總步行不到4分鐘。地址是新宿区大久保2-32-9。"
   }
 };
